3bbbb3d49c55430ba554e6fad0d2a1bd83beb407
Jhonatan Resende <jhonatanlira26@gmail.com>
2026-08-24 17:27:52 -0300

    config: atualiza API_URL para produção

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2,8 +2,7 @@
 // CONFIGURAÇÃO
 // ==================================
 
-// Endereço base da API
-const API_URL = 'http://localhost:3001/api';
+const API_URL = 'https://sistema-de-estoque-gpkb.onrender.com/api';
 
 // ==================================
 // INICIALIZAÇÃO
